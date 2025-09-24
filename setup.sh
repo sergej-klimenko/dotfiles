7a3099bf4d94b1358e8cda805f00652e81298079
sergej-klimenko <sergej.klimenko@gmail.com>
2025-09-24 11:55:25 +0300

    feat: enhance setup script for xcode and homebrew
    Added checks for Xcode CLI tools installation and modified Homebrew installation path.

--- setup.sh
+++ setup.sh
@@ -2,21 +2,42 @@
 
 DOTFILES_REPO=https://github.com/sergej-klimenko/dotfiles.git
 
-BREW_HOME=/usr/local/Homebrew
-if [ "$(arch)" = "arm64" ]; then
-    BREW_HOME=/opt/homebrew
+# See http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
+echo "Checking Xcode CLI tools"
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    tail -n 1 | sed 's/^[^C]* //')
+    echo "Prod: ${PROD}"
+  softwareupdate -i "$PROD" --verbose;
+else
+  echo "Xcode CLI tools OK"
 fi
+
+BREW_HOME=/opt/homebrew
 
 # Install homebrew
 # https://brew.sh/
 if [ ! -d "$BREW_HOME" ]; then
-	echo "Installing Homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	BREW_HOME=~/.local/Homebrew
+	LOCAL_BIN=~/.local/bin
+	if [ ! -d "$BREW_HOME" ]; then
+		echo "Installing Homebrew..."
+		# install as user
+		mkdir -p $BREW_HOME && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $BREW_HOME
+		mkdir -p $LOCAL_BIN && ln -s $BREW_HOME/bin/brew $LOCAL_BIN
+		export PATH="$PATH:$LOCAL_BIN"
+	fi
 fi
 
 if ! command -v git &> /dev/null
 then
-    # Install git to get the brewfile
+    # Install git
 	brew install git
 
 	# Install git credential manager
